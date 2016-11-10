5746e587b3eb7008729e02cf6fa2e324d322aeed
juliakimchung <julhkm@gmail.com>
2016-11-09 22:40:01 -0600

    need delete btn

--- music.js
+++ music.js
@@ -29,24 +29,74 @@ listMusic.addEventListener("click", function() {
 	songList.classList.remove("hidden");
     sidebar.classList.remove("hidden");
 
-	// songList.classList.add("hidden");
-	// sideBar.classList.add("hidden");
-
+	
 })
-
-// body.addEventListener("click", function() {
-// 	addInputBar.classList.add("hidden");
-// 	addInputBar.classList.remove("visible");
-
-// 	songList.classList.toggle("visible");
-// 	sidebar.classList.toggle("visible");
-// 	songList.classList.remove("hidden");
-//     sidebar.classList.remove("hidden");
-// })
+var listOfSongs = document.getElementById("song-list");
+var inputBtn = document.getElementById("inputButton");
+var deleteBtn = document.getElementsByClassName("deleteBtn");
+	inputBtn.addEventListener("click", addNewArtist);
+	
+var songData = {};
 
 
-// if ("keypress" === 13) {
-// 	addInputBar.classList.add("hidden");
-// 	addInputBar.classList.remove("visible");
+function addNewArtist() {
+	var artist = document.getElementById("addArtist");
+	var song = document.getElementById("addSong");
+	var album = document.getElementById("addAlbum")
+
+	var newData = {Song : song.vlaue, Artist: artist.value, Album: album.value};
+
+	//songData = songData.unshift(newData);
+
+	//songDom();
+
+};
+	console.log("songData", songData );
+
+
+
+function songDom() {
+	var newHTML = " ";
+	for (var item in songData) {
+		newHTML += `<div id=song--${item}><p class="songN">${songData[item].Song}</p><p class = "ArtistN">${songData[item].Artist}</p><p class = "AlbumN">${songData[item].Album}</p><input type ="button" class="deleteBtn"></div>`
+	}
+	listOfSongs.innerHTML = newHTML;
+
+	deleteBtnEvent();
+	
+}
+
+function deleteBtnEvent() {
+
+	for (var i = 0; i < deleteBtn.length; i ++) {
+		deleteBtn[i].addEventListener("click", deleteItem);
+	}
+}
+
+// function deleteItem () {
+// 	var 
 // }
+
+function addJsonSongs() {
+	var myRequest = new XMLHttpRequest();
+	myRequest.open("GET", "songs.json");
+	myRequest.send();
+	myRequest.addEventListener("load", addNewSongs );
+
+
+}
+
+function addNewSongs () {
+
+     var newSongData = JSON.parse(this.responseText);
+         newSongData.forEach(function (object){
+         songData.unshift(object);
+         songDom();
+
+});
+
+}
+
+
+
 
